98135a87fa53ac0809334a2ee01c176f85a3f9b6
Бояршинов Константин <costya74@bk.ru>
2022-05-29 18:23:39 +0500

    first commit

--- src/test/java/ru/netology/DataGeneratorTest.java
+++ src/test/java/ru/netology/DataGeneratorTest.java
@@ -1,5 +1,9 @@
 package ru.netology;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -8,6 +12,17 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 
 public class DataGeneratorTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+
     @Test
     void shouldCheckDeliveryDate() {
         open("http://localhost:9999/");
